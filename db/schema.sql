dfcdbc596fad1668770060221e480137756951d0
saq1b786 <sqbrahman17@gmail.com>
2025-11-13 20:02:43 +0000

    created the table in the schema, keeping it simple for now and adding a couple more attributes

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employees (
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone_number VARCHAR(15),
+    job_title VARCHAR(50) NOT NULL
+);
+
